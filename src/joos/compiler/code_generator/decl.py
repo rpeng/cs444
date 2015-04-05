91f1738ba16f099c3a6023e8eb3304494ebb4250
Jiaer Wang <j332wang@uwaterloo.ca>
2015-04-04 22:11:17 -0400

    class outline plus constructors and methods

--- src/joos/compiler/code_generator/decl.py
+++ src/joos/compiler/code_generator/decl.py
@@ -12,16 +12,27 @@ class DeclCodeMixin(object):
         self.writer.OutputLine("; Name for class decl")
         # .bss
         self.writer.OutputLine("; Statics for class decl")
-        self.writer.OutputLine("; Methods for class decl")
 
-        import ipdb; ipdb.set_trace();
-        self.DefaultBehaviour(node)
+        self.writer.OutputLine("; Methods for class decl")
+        self.Visit(node.constructor_decls)
+        self.Visit(node.method_decls)
 
     def VisitInterfaceDecl(self, node):
         self.DefaultBehaviour(node)
 
     def VisitMethodDecl(self, node):
-        self.DefaultBehaviour(node)
+        # link the methods globally
+        method_name = self.namer.Visit(node)
+        self.writer.DefineGlobalLabel(method_name)
+        self.writer.OutputLine('push ebp, esp')
+        self.writer.OutputLine('mov ebp, esp')
+
+        self.writer.OutputLine('; method body')
+        #TODO self.Visit(node.body_block)
+
+        self.writer.OutputLine('leave')
+        self.writer.OutputLine('ret')
+        self.writer.OutputLine('')
 
     def VisitMethodHeader(self, node):
         self.DefaultBehaviour(node)
@@ -30,7 +41,18 @@ class DeclCodeMixin(object):
         self.DefaultBehaviour(node)
 
     def VisitConstructorDecl(self, node):
-        self.DefaultBehaviour(node)
+        # link the methods globally
+        constructor_name = self.namer.Visit(node)
+        self.writer.DefineGlobalLabel(constructor_name)
+        self.writer.OutputLine('push ebp, esp')
+        self.writer.OutputLine('mov ebp, esp')
+
+        self.writer.OutputLine('; constructor body')
+        #TODO self.Visit(node.body)
+
+        self.writer.OutputLine('leave')
+        self.writer.OutputLine('ret')
+        self.writer.OutputLine('')
 
     def VisitVariableDeclarator(self, node):
         self.DefaultBehaviour(node)
